0cfda2eb32afe43c7b618f237a7ef8867907af34
Larissa Duarte Santana <52838539+larissantana27@users.noreply.github.com>
2023-09-01 17:04:04 -0300

    @n1KEHCiX - Creating Project Template

---
+++ src/main/java/com/example/SangueAmigo/SangueAmigoApplication.java
@@ -0,0 +1,12 @@
+package com.example.SangueAmigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangueAmigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangueAmigoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/SangueAmigo/SangueAmigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SangueAmigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangueAmigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
