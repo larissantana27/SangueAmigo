170861a50acb7bcf211642728051e1c4c6356ea4
Larissa Duarte Santana <52838539+larissantana27@users.noreply.github.com>
2023-10-16 18:32:44 -0300

    Defining corsConfiguration Allowed for Integration

--- src/main/java/com/example/SangueAmigo/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/example/SangueAmigo/infrastructure/security/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +27,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .and()
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
